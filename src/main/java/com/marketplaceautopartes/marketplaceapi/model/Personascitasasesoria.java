094b820d78ddf7e624c1f67270bf87ea939bbd78
Yelsin Bañico <neil.banico.27@unsch.edu.pe>
2025-09-17 20:57:28 -0500

    mejorando

---
+++ src/main/java/com/marketplaceautopartes/marketplaceapi/model/Personascitasasesoria.java
@@ -0,0 +1,17 @@
+package com.marketplaceautopartes.marketplaceapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name=)
+public class Personascitasasesoria {
+}
